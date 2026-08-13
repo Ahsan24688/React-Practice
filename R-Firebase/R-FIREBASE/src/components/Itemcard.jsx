f8863e1f5f55945c9ee74f67eb05952f3bfea8da
Ahsan24688 <muhammadahsan9122@gmail.com>
2026-08-13 17:08:28 +0500

    React Practice

--- R-Firebase/R-FIREBASE/src/components/Itemcard.jsx
+++ R-Firebase/R-FIREBASE/src/components/Itemcard.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Itemcard = (props) => {
   return (
     <div className='flex flex-col gap-2 m-2 p-2 rounded-2xl bg-indigo-300'>
-      <img src={props.imageurl} alt="img" className=' h-48 object-contain rounded-xl cursor-pointer w-48 p-2' />
+      <img src={props.imageurl} alt="img" className='w-60 h-50 object-contain rounded-xl cursor-pointer' />
       <h1>Title: {props.producttitle}</h1>
       <p>Desc: {props.Description}</p>
       <p>Price: {props.Price}</p>
